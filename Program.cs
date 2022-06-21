19e55eb5a68a223c51e7803411ea2b0b1b4c4cb0
Alexkand <kandaurovav@mail.ru>
2022-06-22 00:05:39 +0300

    задача2

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace задача2
+{
+    internal class Program
+    {
+        public static string NumberDecimalSeparator { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите три числа, десятичные знаки отделяйте при помощи - .");
+
+            string a;
+
+            string d;
+
+            string e;
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo();
+
+            {
+                NumberDecimalSeparator = ".";
+            }
+
+            a = Console.ReadLine();
+
+            d = Console.ReadLine();
+
+            e = Console.ReadLine();
+
+            double v = Convert.ToDouble(a, numberFormatInfo);
+
+            double s = Convert.ToDouble(d, numberFormatInfo);
+
+            double x = Convert.ToDouble(e, numberFormatInfo);
+
+            double Result = (double) v + s + x;
+
+            double Result1 = (double) v * s * x;
+
+            Console.WriteLine("Сумма чисел = " + Result);
+
+            Console.WriteLine("Произведение чисел = " + Result1);
+        }
+    }
+}
